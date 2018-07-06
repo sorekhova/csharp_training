fa6df899e31a32aa3217eda5f2d4bee5395de68a
sorekhova <svetlana.orekhova@emerson.com>
2018-07-06 18:06:55 +0300

    Creation of the TestBase class

---
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+
+
+        [Test]
+        public void GroupRemovalTest()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin","secret"));
+            GoToGroupsPage();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupsPage();
+            LogOut();
+        }
+
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/TestBase.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+        private StringBuilder verificationErrors;
+        protected string baseURL;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.UseLegacyImplementation = true;
+            options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            driver = new FirefoxDriver(options);
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        protected void GoToHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/group.php");
+        }
+
+        protected void Login(AccountData accountdata)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(accountdata.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(accountdata.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        protected void LogOut()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        protected void FillGroupForm(GroupData group)
+        {
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        protected void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        protected void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+ 
+
+        protected void SubmitCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        protected void ReturnToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        protected void RemoveGroup()
+        {
+            driver.FindElement(By.Name("delete")).Click();
+        }
+
+        protected void SelectGroup(int index)
+        {
+            driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
+        }
+
+        protected void FillEntry(EntryData entry)
+        {
+
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(entry.Firstname);
+
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(entry.Middlename);
+
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(entry.Lastname);
+
+            driver.FindElement(By.Name("nickname")).Clear();
+            driver.FindElement(By.Name("nickname")).SendKeys(entry.Nickname);
+
+            driver.FindElement(By.Name("photo")).Clear();
+            driver.FindElement(By.Name("photo")).SendKeys(entry.Photo);
+
+            driver.FindElement(By.Name("title")).Clear();
+            driver.FindElement(By.Name("title")).SendKeys(entry.Title);
+
+            driver.FindElement(By.Name("company")).Clear();
+            driver.FindElement(By.Name("company")).SendKeys(entry.Company);
+
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(entry.Address);
+
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys(entry.Home);
+
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(entry.Mobile);
+
+            driver.FindElement(By.Name("work")).Clear();
+            driver.FindElement(By.Name("work")).SendKeys(entry.Work);
+
+            driver.FindElement(By.Name("fax")).Clear();
+            driver.FindElement(By.Name("fax")).SendKeys(entry.Fax);
+
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys(entry.Email);
+
+            driver.FindElement(By.Name("email2")).Clear();
+            driver.FindElement(By.Name("email2")).SendKeys(entry.Email2);
+
+            driver.FindElement(By.Name("email3")).Clear();
+            driver.FindElement(By.Name("email3")).SendKeys(entry.Email3);
+
+            driver.FindElement(By.Name("homepage")).Clear();
+            driver.FindElement(By.Name("homepage")).SendKeys(entry.Homepage);
+
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText(entry.Bday);
+
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText(entry.Bmonth);
+
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys(entry.Byear);
+
+            new SelectElement(driver.FindElement(By.Name("aday"))).SelectByText(entry.Aday);
+
+            new SelectElement(driver.FindElement(By.Name("amonth"))).SelectByText(entry.Amonth);
+
+            driver.FindElement(By.Name("ayear")).Clear();
+            driver.FindElement(By.Name("ayear")).SendKeys(entry.Ayear);
+
+            new SelectElement(driver.FindElement(By.Name("new_group"))).SelectByText(entry.New_group);
+
+            driver.FindElement(By.Name("address2")).Clear();
+            driver.FindElement(By.Name("address2")).SendKeys(entry.Address2);
+
+            driver.FindElement(By.Name("phone2")).Clear();
+            driver.FindElement(By.Name("phone2")).SendKeys(entry.Phone2);
+
+            driver.FindElement(By.Name("notes")).Clear();
+            driver.FindElement(By.Name("notes")).SendKeys(entry.Notes);
+
+        }
+
+        protected void AddNewEntry()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+    }
+}
