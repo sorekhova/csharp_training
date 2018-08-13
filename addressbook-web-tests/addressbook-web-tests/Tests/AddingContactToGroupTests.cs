636e0a8b45a100fad5d77766a97943d47dc0437c
sorekhova <svetlana.orekhova@emerson.com>
2018-08-13 11:59:54 +0300

    Updated adding to group

--- addressbook-web-tests/addressbook-web-tests/Tests/AddingContactToGroupTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/AddingContactToGroupTests.cs
@@ -13,46 +13,39 @@ namespace WebAddressbookTests
         public void TestAddingContactToGroup()
         {
             int contactIndex = 2;
-            int groupIndex = 0;
-            ContactData addContact = new ContactData("Иван", "Сергеев");
+            int groupIndex = 8;
+
+            ContactData addContact = new ContactData("Юрий", "Сергеев");
             app.Contacts.IsContactPresent(contactIndex, app.Contacts.bySelected, addContact);
             ContactData contact = ContactData.GetAll()[contactIndex];
 
-            GroupData addData = new GroupData("asd");
+            GroupData addData = new GroupData("my");
             app.Groups.IsGroupPresent(groupIndex, app.Groups.bySelected, addData);
-//          GroupData group = GroupData.GetAll()[groupIndex];
 
             List<GroupData> groups = GroupData.GetAll().ToList();
-            List<GroupContactRelation> enteredToGroups = contact.ContactEnteredToGroups(contact.Id);
-            if (enteredToGroups.Count == groups.Count)
+            GroupData group = groups[groupIndex];
+            
+            if (group.ContactEnteredToGroup(contact.Id, group.Id))
             {
                 app.Contacts.Create(addContact);
                 contact = ContactData.GetLastContact();
             }
 
-            for (int i = 0; i < groups.Count; ++i)
-            {
-                GroupData group = GroupData.GetAll()[i];
+            List<ContactData> oldList = group.GetContacts();
+            //           List<ContactData> oldList = group.GetContact(contact.Id);
+            //           ContactData contact = ContactData.GetAll().Except(oldList).First();
 
-                List<ContactData> oldList = group.GetContacts();
-                if (!group.ContactEnteredToGroup(contact.Id, group.Id))
-                {
-                    //           List<ContactData> oldList = group.GetContact(contact.Id);
+            //actions
+            app.Contacts.AddContactsToGroup(contact, group);
 
-                    //           ContactData contact = ContactData.GetAll().Except(oldList).First();
+            //compare
+            List<ContactData> newList = group.GetContacts();
+            oldList.Add(contact);
+            oldList.Sort();
+            newList.Sort();
 
-                    //actions
-                    app.Contacts.AddContactsToGroup(contact, group);
-                    //compare
+            Assert.AreEqual(oldList, newList);
 
-                    List<ContactData> newList = group.GetContacts();
-                    oldList.Add(contact);
-                    oldList.Sort();
-                    newList.Sort();
-
-                    Assert.AreEqual(oldList, newList);
-                }
-            }
         }
 
         [Test]
