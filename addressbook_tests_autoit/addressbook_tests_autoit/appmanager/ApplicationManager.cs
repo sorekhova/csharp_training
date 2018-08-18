381c0b292e201e693ded98d463894e876d3f62fe
sorekhova <svetlana.orekhova@emerson.com>
2018-08-18 22:00:16 +0300

    New project for AutoIt

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/ApplicationManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoItX3Lib;
+
+
+namespace addressbook_tests_autoit
+{
+    
+    public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        private AutoItX3 aux;
+        private GroupHelper groupHelper;
+        public ApplicationManager()
+        {
+            
+            aux = new AutoItX3();
+
+            aux.Run(@"c:\Temp\FreeAddressBookPortable\AddressBook.exe", @"c:\Temp\FreeAddressBookPortable\",aux.SW_SHOW);
+ 
+            aux.WinWait(WINTITLE,"",500);
+ //           aux.WinActivate(WINTITLE);
+            aux.WinWaitActive(WINTITLE,"",500);
+
+            groupHelper = new GroupHelper(this);
+        }
+
+        public void Stop()
+        {
+            aux.Sleep(1000);
+                       aux.ControlClick(WINTITLE, "&Exit", "WindowsForms10.BUTTON.app.0.2c908d510");
+         //   aux.ControlClick(WINTITLE, "&Exit", "");
+
+        }
+
+        public AutoItX3 Aux
+        {
+            get
+            {
+                return aux;
+            }
+        }
+        public GroupHelper Groups
+        {
+            get { return groupHelper; }
+        }
+    }
+}
