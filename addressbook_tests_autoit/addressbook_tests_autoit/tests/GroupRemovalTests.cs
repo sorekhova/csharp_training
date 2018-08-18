017499e04d1f0265f337bd476be71925d89eeb4b
sorekhova <svetlana.orekhova@emerson.com>
2018-08-18 22:24:23 +0300

    AutoIT

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+
+        [Test]
+        public void TestGroupRemoval()
+        {
+            int index = 3;
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            if (oldGroups.Count < index)
+            {
+                for(int i = oldGroups.Count; i<index; i++)
+                {
+                    GroupData newGroup = new GroupData()
+                    {
+                        Name = "ngA"
+                    };
+
+                    app.Groups.Add(newGroup);
+                    oldGroups.Add(newGroup);
+                }
+            }
+
+            app.Groups.Remove(index-1);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(index-1);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
