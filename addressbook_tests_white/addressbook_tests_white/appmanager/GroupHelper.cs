87afc991520472728a034888948dc11ca9bf08d5
sorekhova <svetlana.orekhova@emerson.com>
2018-08-19 15:59:27 +0300

    White

---
+++ addressbook_tests_white/addressbook_tests_white/appmanager/GroupHelper.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using TestStack.White;
+using TestStack.White.WindowsAPI;
+using TestStack.White.InputDevices;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems.WindowItems;
+using System.Windows.Automation;
+
+namespace addressbook_tests_white
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public static string DELETEGROUPWINTITLE = "Delete group";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            foreach (TreeNode item in root.Nodes)
+            {
+                list.Add(new GroupData()
+                {
+                    Name = item.Text
+                });
+            };
+
+            CloseGroupsDialogue(dialogue);
+
+            return list;
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            Window dialogue = OpenGroupsDialogue();
+
+            dialogue.Get<Button>("uxNewAddressButton").Click();
+            TextBox textBox = (TextBox) dialogue.Get(SearchCriteria.ByControlType(ControlType.Edit));
+            textBox.Enter(newGroup.Name);
+            Keyboard.Instance.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN);
+
+            CloseGroupsDialogue(dialogue);
+            return;
+        }
+
+        public void Remove(int groupNumber)
+        {
+
+            Window dialogue = OpenGroupsDialogue();
+
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            root.Nodes[groupNumber].Select();
+            
+            dialogue.Get<Button>("uxDeleteAddressButton").Click();
+            dialogue.Get<Button>("uxOKAddressButton").Click();
+            //         dialogue.Get<Window>("DeleteGroupForm").Get<Button>("uxOKAddressButton").Click();
+            CloseGroupsDialogue(dialogue);
+            return;
+        }
+
+        private void CloseGroupsDialogue(Window dialogue)
+        {
+            dialogue.Get<Button>("uxCloseAddressButton").Click();
+        }
+
+        private Window OpenGroupsDialogue()
+        {
+            manager.MainWindow.Get<Button>("groupButton").Click();
+            return manager.MainWindow.ModalWindow(GROUPWINTITLE);
+        }
+    }
+}
---
+++ addressbook_tests_white/addressbook_tests_white/appmanager/HelperBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace addressbook_tests_white
+{
+    public class HelperBase
+    {
+        protected ApplicationManager manager;
+
+
+
+        public HelperBase(ApplicationManager manager)
+        {
+            this.manager = manager;
+        }
+
+
+    }
+}
