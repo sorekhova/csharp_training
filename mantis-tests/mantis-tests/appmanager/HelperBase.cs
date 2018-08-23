a3c89521dcaa0adaaa9e76823f1f52ae29ae30cd
sorekhova <svetlana.orekhova@emerson.com>
2018-08-23 18:23:18 +0300

    New project Mantis added

---
+++ mantis-tests/mantis-tests/appmanager/HelperBase.cs
@@ -0,0 +1,93 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace mantis_tests
+{
+    public class HelperBase
+    {
+        protected IWebDriver driver;
+        protected ApplicationManager manager;
+        private bool acceptNextAlert = true;
+        public string bySelected = "(//input[@name='selected[]'])";
+        public string byDetails = "(//img[@alt='Details'])";
+        public string byEdit = "(//img[@alt='Edit'])";
+        public HelperBase(ApplicationManager manager)
+        {
+            this.manager = manager;
+            driver = manager.Driver;
+        }
+
+        public void Type(By locator, string text)
+        {
+            if (text != null)
+            {
+                driver.FindElement(locator).Clear();
+                driver.FindElement(locator).SendKeys(text);
+            }
+        }
+
+        public bool Value(By locator, string text)
+        {
+             return (driver.FindElement(locator).GetAttribute("value").ToString() == text);
+        }
+
+        public string Value(By locator)
+        {
+            return driver.FindElement(locator).GetAttribute("value").ToString() ;
+        }
+
+        public bool IsItemPresent(int index, string type)
+        {
+            return IsElementPresent(By.XPath(type + "[" + (index + 1) + "]"));
+        }
+        public bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        public string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+
+    }
+}
---
+++ mantis-tests/mantis-tests/tests/TestBase.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace mantis_tests
+{
+    public class TestBase
+    {
+        public static bool PERFORM_LONG_UI_CHECKS = true;
+        protected ApplicationManager app;
+
+        [SetUp]
+        public void SetupApplicationManager()
+        {
+            app = ApplicationManager.GetInstance();
+        }
+
+        public static Random rnd = new Random();
+        public static string GenerateRandomString(int max)
+        {
+            int l = Convert.ToInt32(rnd.NextDouble() * max);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < l; i++)
+            {
+                builder.Append(Convert.ToChar(32 + Convert.ToInt32(rnd.NextDouble() * 65)));
+            }
+
+            return builder.ToString();
+        }
+
+        public static string GenerateRandomInt(int max)
+        {
+            int l = Convert.ToInt32(rnd.NextDouble() * max);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < l; i++)
+            {
+                builder.Append(Convert.ToChar(48 + Convert.ToInt32(rnd.NextDouble() * 9)));
+            }
+            
+            return builder.ToString();
+        }
+
+        public static string GenerateRandomDay()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            int nextdata = Convert.ToInt32(rnd.NextDouble());
+            if (nextdata == 0)
+            { builder.Append("-"); }
+            else
+            {
+                builder.Append(Convert.ToChar(48 + nextdata * 9)); 
+
+                if (Convert.ToInt32(builder.ToString()) == 3)
+                { builder.Append(Convert.ToChar(48 + Convert.ToInt32(rnd.NextDouble() * 1))); }
+                else
+                {
+                    if (Convert.ToInt32(builder.ToString()) == 1 || Convert.ToInt32(builder.ToString()) == 2)
+                    { builder.Append(Convert.ToChar(48 + Convert.ToInt32(rnd.NextDouble() * 9))); }
+                }
+            }
+            return builder.ToString();
+        }
+
+        public static string GenerateRandomMonth()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append(Convert.ToChar(48 + Convert.ToInt32(rnd.NextDouble() * 9)));
+            if (Convert.ToInt32(builder.ToString()) == 1)
+            { builder.Append(Convert.ToChar(48 + Convert.ToInt32(rnd.NextDouble() * 2))); }
+
+            return builder.ToString();
+        }
+
+        public static string GenerateRandomYear()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+            {
+                builder.Append(Convert.ToChar(48 + Convert.ToInt32(rnd.NextDouble() * 9))); 
+            }
+
+            return builder.ToString();
+        }
+    }
+}
